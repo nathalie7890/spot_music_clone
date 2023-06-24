d90304de68bb27b9758ce920861a5300bb2dbfbf
nathalie <103376453+nathalie7890@users.noreply.github.com>
2023-06-25 00:06:56 +0800

    completed song upload

---
+++ components/Input.tsx
@@ -0,0 +1,24 @@
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, disabled, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={twMerge(
+          `flex w-full rounded-md bg-neutral-700 border border-transparent px-3 py-3 text-sm file:border-0 file:text-sm file:bg-transparent file:font-mediumn placeholder:text-neutral-400 disabled:opacity-50 focus:outline-none`,
+          className
+        )}
+        disabled={disabled}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
+export default Input;
